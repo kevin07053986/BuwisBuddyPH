0173175005ffec020da8cc382eba87c82e42a157
Kevin Mahusay <kevinmahusay22@gmail.com>
2024-03-20 19:00:39 +0800

    Initial-Commit_03/20

--- app/src/main/java/com/mab/buwisbuddyph/DashboardActivity.kt
+++ app/src/main/java/com/mab/buwisbuddyph/DashboardActivity.kt
@@ -12,10 +12,14 @@ class DashboardActivity : AppCompatActivity() {
 
         // Retrieve username from intent extras
         val email = intent.getStringExtra("EMAIL_KEY")
+        val password = intent.getStringExtra("PASSWORD_KEY")
 
         // Display username
         val emailTextView = findViewById<TextView>(R.id.textView)
-        emailTextView.text = "Welcome, $email"
+        emailTextView.text = "Email: $email"
+
+        val passwordTextView = findViewById<TextView>(R.id.textView1) // Use textView1 for the password TextView
+        passwordTextView.text = "Password: $password"
 
     }
 }
--- app/src/main/java/com/mab/buwisbuddyph/LoginActivity.kt
+++ app/src/main/java/com/mab/buwisbuddyph/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.mab.buwisbuddyph
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -13,8 +15,21 @@ class LoginActivity : AppCompatActivity() {
 
         SignInButton.setOnClickListener {
             val email = editTextEmail.text.toString()
+            if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                // Display an error message
+                Toast.makeText(this, "Invalid email address", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener // Exit the function if the email is invalid
+            }
+            val password = editTextPassword.text.toString()
+            if (password.isEmpty()) {
+                Toast.makeText(this, "Please enter your password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener // Exit the function if the password is empty
+            }
+
+
             val intent = Intent(this, DashboardActivity::class.java)
             intent.putExtra("EMAIL_KEY", email)
+            intent.putExtra("PASSWORD_KEY", password)
             startActivity(intent)
         }
     }
